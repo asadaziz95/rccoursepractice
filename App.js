b492e0b9879a25ac4bc2bf7144f15bd548534393
asadaziz95 <asadazizjs@gmail.com>
2019-11-15 19:17:31 +0500

    navigation added

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -14,55 +6,14 @@ import {
   View,
   Text,
   StatusBar,
-  FlatList,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-//import ListView from './src/components/ListView/index.js';
-
-const ListView = () => {
-  const friends = [
-    {name: 'Friend #1', age: '23'},
-    {name: 'Friend #2', age: '21'},
-    {name: 'Friend #3', age: '19'},
-    {name: 'Friend #4', age: '21'},
-    {name: 'Friend #5', age: '22'},
-    {name: 'Friend #6', age: '23'},
-    {name: 'Friend #7', age: '24'},
-    {name: 'Friend #8', age: '25'},
-    {name: 'Friend #9', age: '26'},
-    {name: 'Friend #10', age: '27'},
-    {name: 'Friend #11', age: '28'},
-  ];
-  return (
-    <FlatList
-      // horizontal
-      // showsHorizontalScrollIndicator={false}
-      data={friends}
-      keyExtractor={friends => friends.name}
-      renderItem={({item}) => {
-        return (
-          <Text style={styles.textStyle}>
-            {'Name :' + item.name + 'Age :' + item.age}
-          </Text>
-        );
-      }}
-    />
-  );
-};
+import {CreateAppContainer} from 'react-navigation';
+import 
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <ListView />
     </>
   );
 };
--- src/components/ListView/ListView.jsx
+++ src/components/ListView/ListView.jsx
@@ -3,23 +3,34 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListView = () => {
     const friends = [
-        { name: 'Friend #1' },
-        { name: 'Friend #2' },
-        { name: 'Friend #3' },
-        { name: 'Friend #4' },
-        { name: 'Friend #5' },
-        { name: 'Friend #6' },
-        { name: 'Friend #7' },
-        { name: 'Friend #8' },
-        { name: 'Friend #9' },
-        { name: 'Friend #10' },
-        { name: 'Friend #11' },
+      {name: 'Friend #1', age: '23'},
+      {name: 'Friend #2', age: '21'},
+      {name: 'Friend #3', age: '19'},
+      {name: 'Friend #4', age: '21'},
+      {name: 'Friend #5', age: '22'},
+      {name: 'Friend #6', age: '23'},
+      {name: 'Friend #7', age: '24'},
+      {name: 'Friend #8', age: '25'},
+      {name: 'Friend #9', age: '26'},
+      {name: 'Friend #10', age: '27'},
+      {name: 'Friend #11', age: '28'},
     ];
     return (
-        <FlatList data={friends} renderItem={(element) => {return <Text>{element}</Text>}}/>
-
-    )
-}
+      <FlatList
+        // horizontal
+        // showsHorizontalScrollIndicator={false}
+        data={friends}
+        keyExtractor={friends => friends.name}
+        renderItem={({item}) => {
+          return (
+            <Text style={styles.textStyle}>
+              {'Name :' + item.name + 'Age :' + item.age}
+            </Text>
+          );
+        }}
+      />
+    );
+  };
 
 const styles = StyleSheet.create({
 
